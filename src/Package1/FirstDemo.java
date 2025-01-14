3b6c89391ba77e616db9b1d6e034b13484d71de6
Hemant Shukla <hbshukla@gmail.com>
2025-01-14 23:12:38 +0530

    FirstDemo file added

---
+++ src/Package1/FirstDemo.java
@@ -0,0 +1,16 @@
+package Package1;
+
+public class FirstDemo 
+{
+	public static void main(String[]args)
+	{
+		int no=123;
+		System.out.println("Hello selenium Students"+no);
+		System.out.println("This is Tops Techonologies");
+		System.out.println("Mo:123456");
+		System.out.println("Hello All");
+		System.out.println(no+no);
+		
+	}
+
+}
