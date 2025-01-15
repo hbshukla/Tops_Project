a083410fd6505a5b7291a3aed5098bf26ee0115d
Hemant Shukla <hbshukla@gmail.com>
2025-01-15 23:31:27 +0530

    changes committed

--- src/Package1/Basic_Pyramid.java
+++ src/Package1/Basic_Pyramid.java
@@ -1,25 +1,20 @@
 package Package1;
 
 public class Basic_Pyramid {
-	public static void main(String[] args)
-	{
-		for (int i=1; i<=5; i++)
-		{
-			//Print leading space 
-			for (int s=1; s<=5-i; s++)
-			{
+	public static void main(String[] args) {
+		for (int i = 1; i <= 5; i++) {
+			// Print leading space
+			for (int s = 1; s <= 5 - i; s++) {
 				System.out.print(" ");
 			}
-			
-			//Print Numbers
-			for (int n=1;n<=i;n++) 
-			{
-				System.out.print(n + " ");	
+
+			// Print Numbers
+			for (int n = 1; n <= i; n++) {
+				System.out.print("*" + " ");
 			}
 			// Move to Next Line
 			System.out.println();
-		
-		}
 
+		}
 	}
 }
