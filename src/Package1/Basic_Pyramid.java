f1b09deec5ca36e1eb683492db3fa7e04935c745
Hemant Shukla <hbshukla@gmail.com>
2025-01-15 22:10:59 +0530

    New Files added

---
+++ src/Package1/Basic_Pyramid.java
@@ -0,0 +1,25 @@
+package Package1;
+
+public class Basic_Pyramid {
+	public static void main(String[] args)
+	{
+		for (int i=1; i<=5; i++)
+		{
+			//Print leading space 
+			for (int s=1; s<=5-i; s++)
+			{
+				System.out.print(" ");
+			}
+			
+			//Print Numbers
+			for (int n=1;n<=i;n++) 
+			{
+				System.out.print(n + " ");	
+			}
+			// Move to Next Line
+			System.out.println();
+		
+		}
+
+	}
+}
