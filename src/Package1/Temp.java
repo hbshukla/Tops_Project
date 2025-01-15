7ea510c5a373192a2dc617a614425fbc76426fd4
Hemant Shukla <hbshukla@gmail.com>
2025-01-15 22:10:59 +0530

    New Files added

---
+++ src/Package1/Temp.java
@@ -0,0 +1,25 @@
+package Package1;
+
+public class Temp {
+	public static void main(String[] args)
+	{
+		for (int i=1; i<=5; i++)
+		{
+			//Print leading space 
+			for (int s=1; s<=6-i; s++)
+			{
+				System.out.print(s);
+			}
+			
+			//Print Numbers
+			for (int n=1;n<=i;n++) 
+			{
+			//	System.out.print(n + " ");	
+			}
+			// Move to Next Line
+			System.out.println();
+		
+		}
+
+	}
+}
