eeb321466ec7f973929ed5a3a673b7aa1f47b8db
Hemant Shukla <hbshukla@gmail.com>
2025-01-24 19:19:56 +0530

    com3

--- src/Package1/HelloWorld.java
+++ src/Package1/HelloWorld.java
@@ -4,19 +4,9 @@ public class HelloWorld {
 	public static void main(String[] args)
 	{
 		System.out.println("Hello World");
-<<<<<<< HEAD
-=======
 		//comment1-by temp
 		//comment2-by temp
-<<<<<<< HEAD
->>>>>>> temp
-=======
-		//comment3-by temp
->>>>>>> temp
-
-		//comment1-by temp
-		//comment2-by temp
-		
+		//comment3-by temp	
 	}
 
 }
